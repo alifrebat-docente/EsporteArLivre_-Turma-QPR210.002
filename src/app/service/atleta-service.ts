cc9cc882dcbfb2c16e09c47c64f32b09cc2e244a
WallaceOliveiradosSantos <wallace@docente.senai.br>
2026-08-24 20:51:04 -0300

    Criado a função de que recebe a data no formato aaaa-MM-dd, do tipo string, e realiza o cálculo da idade

--- src/app/service/atleta-service.ts
+++ src/app/service/atleta-service.ts
@@ -13,21 +13,23 @@ export class AtletaService {
   //ADICIONAR NA API
   adicionarAtleta(atleta: Atleta): Observable<Atleta> {
     const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
+    //const urlApi = `http://127.0.0.1:8000/atleta/`
 
     return this.http.post<Atleta>(urlApi, atleta)
   }
 
   //LISTAR ATLETAS NA API
   listarAtletas(): Observable<Atleta[]> {
-    /*    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`*/
+    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
 
-   const  urlApi = `http://127.0.0.1:8000/pessoa/`
+    //const  urlApi = `http://127.0.0.1:8000/atleta/`
 
     return this.http.get<Atleta[]>(urlApi)
   }
 
   //LISTAR ATLETA
   listarAtleta(idAtleta: number): Observable<Atleta> {
+    //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
     const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
 
     return this.http.get<Atleta>(urlApi)
@@ -36,6 +38,7 @@ export class AtletaService {
   //EXCLUIR NA API
   exluirAtleta(atleta: Atleta): Observable<Atleta> {
     const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+    //const urlApi = `http://127.0.0.1:8000/atleta/${atleta.id}`
 
     return this.http.delete<Atleta>(urlApi)
   }
@@ -43,6 +46,8 @@ export class AtletaService {
   //ALTERAR NA API
   alterarAtleta(atleta: Atleta): Observable<Atleta> {
     const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+
+    //const urlApi = `http://127.0.0.1:8000/atleta/${atleta.id}`
 
     return this.http.put<Atleta>(urlApi, atleta)
   }
@@ -52,6 +57,22 @@ export class AtletaService {
     const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
 
     return this.http.put<Atleta>(urlApi, atleta)
+  }
+
+  //CALCULAR A IDADE
+  //aaaa-MMM-dd - 2026-08-24
+  calcularIdade(data_nascimento: string): number {
+    const dt_nascimento = new Date(data_nascimento + "T00:00:00")
+    const hoje = new Date()
+
+    let idade = hoje.getFullYear() - dt_nascimento.getFullYear()
+    const resp_calc_mes = hoje.getMonth() - dt_nascimento.getMonth()
+
+    if (resp_calc_mes < 0 || (resp_calc_mes === 0 && hoje.getDate() < dt_nascimento.getDate())) {
+      idade--
+    }
+
+    return idade
   }
 
   /*
