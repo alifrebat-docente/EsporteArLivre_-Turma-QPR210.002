7e3014c65fb14672a46a52578b0f200d6227bd66
WallaceOliveiradosSantos <wallace@docente.senai.br>
2026-08-26 20:39:59 -0300

    Criado o atributo IMC e as funções calcIMC e SitIMC

--- src/app/component/atleta/atleta-lista-component/atleta-lista-component.ts
+++ src/app/component/atleta/atleta-lista-component/atleta-lista-component.ts
@@ -14,6 +14,7 @@ export class AtletaListaComponent {
   //DECLARAÇÃO ARRAY DO TIPO PESSOA
   //listaAtletas: Atleta[] = []
   listaAtletas = signal<Atleta[]>([])
+  imc = 0
 
   //DECLARAÇÃO CONSTRUTOR
   constructor(private router: Router, private http: AtletaService) { }
@@ -40,35 +41,44 @@ export class AtletaListaComponent {
   }
 
   //EXCLUIR ATLETA
-  excluirAtleta(atleta: Atleta){
-    if(confirm(`Deseja excluir ${atleta.nome} da competição? `)){
+  excluirAtleta(atleta: Atleta) {
+    if (confirm(`Deseja excluir ${atleta.nome} da competição? `)) {
       this.http.exluirAtleta(atleta)
-      .subscribe({
-        next:(dados)=>{
-           this.listaAtletas.update(elem =>
-            elem.filter(a => a.id !== atleta.id)
-          );
-          
-          console.log('Atleta excluído com Sucesso ', dados)
-        },
-        error: (msgErro) => {
-          console.log("Erro ao Excluir  o atleta ", msgErro)
-        }
-      })
+        .subscribe({
+          next: (dados) => {
+            this.listaAtletas.update(elem =>
+              elem.filter(a => a.id !== atleta.id)
+            );
+
+            console.log('Atleta excluído com Sucesso ', dados)
+          },
+          error: (msgErro) => {
+            console.log("Erro ao Excluir  o atleta ", msgErro)
+          }
+        })
 
     }
-    
+
     this.ngOnInit()
   }
 
   //ALTERAR DADOS
-  buscarPessoa(idAtleta: Atleta){
+  buscarPessoa(idAtleta: Atleta) {
     this.router.navigate(['/cadastroatleta', idAtleta.id])
   }
 
-  calcIdade(data_nascimento: string){
+  calcIdade(data_nascimento: string) {
     return this.http.calcularIdade(data_nascimento)
   }
 
-  
+  calcImc(atleta: Atleta) {
+    this.imc = this.http.calcularIMC(atleta)
+    return this.imc
+  }
+
+  sitIMC(imc: number) {
+    return this.http.situacaoIMC(imc)
+  }
+
+
 }//FIM COMPONENT AtletaListaComponent
